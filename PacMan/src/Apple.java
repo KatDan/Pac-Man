0103cdce6b03141da610e92383873a89e176ad34
katdan <kd9nlmku8gfe>
2020-05-20 14:13:35 +0200

    opravene konstanty

--- PacMan/src/Apple.java
+++ PacMan/src/Apple.java
@@ -2,12 +2,14 @@ import java.awt.*;
 
 public class Apple extends Rectangle {
 
+    final static int APPLESIZE = 8;
+
     public Apple(int x, int y){
-        setBounds(x,y,32,32);
+        setBounds(x,y,Level.BLOCKSIZE,Level.BLOCKSIZE);
     }
 
     public void render(Graphics g){
         g.setColor(Color.gray);
-        g.fillRect(x + 12,y + 12,8,8);
+        g.fillRect(x + (Level.BLOCKSIZE-APPLESIZE)/2,y + (Level.BLOCKSIZE-APPLESIZE)/2,APPLESIZE,APPLESIZE);
     }
 }
